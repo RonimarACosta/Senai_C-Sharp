9b003d1191b05b13faae10bd84974f712e14d86c
Ronimar A Costa <ronimarantonio@hotmail.com>
2023-10-01 22:28:29 -0300

    c-charp_meu

---
+++ MVC-COM-BANCO/LHPet/LHPet/Models/Contexto.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace LHPet.Models
+{
+    public class Contexto : DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> options) : base(options)
+        {
+        }
+        public DbSet<Cliente> Cliente { get; set; }
+    }
+}
+
